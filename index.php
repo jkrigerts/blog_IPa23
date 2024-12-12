becf7b102b174562e85559a9ebce62aa70a3df74
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-12-12 14:04:12 +0200

    02. Datubāzes klase ar konfigurāciju datni 12.12.24

--- index.php
+++ index.php
@@ -1,28 +1,17 @@
 <?php
 
 require "functions.php";
+require "Database.php";
+
+$config = require("config.php");
 
 echo "Hi there <br>";
+$db = new Database($config["database"]);
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
+// $comments = $db->query("SELECT * FROM comments")->fetchAll(PDO::FETCH_ASSOC);
+// $user = $db->query("SELECT * FROM users WHERE user_id = $id")->fetch(PDO::FETCH_ASSOC);
+// $db->query("INSERT INTO posts ...");
 
-// 1. Datubāze ✅
-// 2. Savienot PHP ar datubāzi ✅
-
-
-// Ar PDO pieslēgties datubāzei
-// data source name
-$dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog_IPa23;charset=utf8mb4";
-// PHP data object
-$pdo = new PDO($dsn); // Šeit vajadzētu ķert kļūdas
-
-// Sagatavot vaicājumu
-$statement = $pdo->prepare("SELECT * FROM posts");
-// Izpildīt vaicājumu
-$statement->execute();
-
-// Dabūt bloga ierakstus
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// Ar foreach izvadīt visus postus.
 
 echo "<ul>";
   foreach ($posts as $post) {
