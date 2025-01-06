11364201d8a712f7230045d0c2a07c398ed0c140
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2025-01-06 09:48:07 +0200

    CHECKPOINT 06.01.25

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
 
+// Mērķis: izveidot ierakstu meklētāju
+// 1. Izveidot HTML formu ar input, submit pogu. ✅
+// 2. PHP saņem no formas un ko tālāk?
+// 3. Izveidot vaicājumu uz datubāzi
+
 require "functions.php";
 require "Database.php";
 
 $config = require("config.php");
 
-echo "Hi there <br>";
+echo "<h1>Blogs</h1>";
 $db = new Database($config["database"]);
 $posts = $db->query("SELECT * FROM posts")->fetchAll();
-// $comments = $db->query("SELECT * FROM comments")->fetchAll(PDO::FETCH_ASSOC);
-// $user = $db->query("SELECT * FROM users WHERE user_id = $id")->fetch(PDO::FETCH_ASSOC);
-// $db->query("INSERT INTO posts ...");
+// SELECT * FROM posts WHERE content LIKE "To kas ir search_query";
+
+if (isset($_GET["search_query"]) && $_GET["search_query"] != "") {
+  // TODO: Meklēšanas loģika
+  dd("SELECT * FROM posts WHERE content LIKE '" . $_GET["search_query"] . "';");
+  $posts = $db->query("SELECT * FROM posts WHERE content LIKE " . $_GET["search_query"])->fetchAll();
+};
+
+// Meklēšanas forma
+// POST - ja maina datu bāzē saturu
+// GET - ja vienkārši atlasa datus
+echo "<form>";
+echo "<input name='search_query' />";
+echo "<button>Meklēt</button>";
+echo "</form>";
 
 
 echo "<ul>";
