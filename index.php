18d19f3ce90b58ca04e19d1b551afb76398a3094
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2025-01-13 08:18:53 +0200

    06. Vienkāršs maršrutētājs 09.01.25

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-// Mērķis: izveidot ierakstu meklētāju
-// 1. Izveidot HTML formu ar input, submit pogu. ✅
-// 2. PHP saņem no formas un ko tālāk?
-// 3. Izveidot vaicājumu uz datubāzi
-
 require "functions.php";
 require "Database.php";
 
@@ -12,16 +7,4 @@ $config = require("config.php");
 
 $db = new Database($config["database"]);
 
-$sql = "SELECT * FROM posts";
-$params = [];
-if (isset($_GET["search_query"]) && $_GET["search_query"] != "") {
-  // Meklēšanas loģika
-  $search_query = "%" . $_GET["search_query"] . "%";
-  $sql .= " WHERE content LIKE :search_query;"; // Bind parameter
-  $params = ["search_query" => $search_query];
-}
-
-$posts = $db->query($sql, $params)->fetchAll();
-$pageTitle = "Blogi";
-
-require "views/index.view.php";
+require "router.php";
--- views/components/navbar.php
+++ views/components/navbar.php
@@ -2,7 +2,7 @@
   <nav>
     <ul>
       <li><a href="/">Blogi</a></li>
-      <li><a href="story.php">Par mums</a></li>
+      <li><a href="about">Par mums</a></li>
     </ul>
   </nav>
 </header>
